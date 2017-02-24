8442e0af93d7f976b696b392162ac98043616acb
CoderJ <jixin1006@gmail.com>
2017-02-24 14:27:28 +0800

    fixed bug

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,63 @@
 var express = require('express');
 var router = express.Router();
 var amazonCrawler = require(__base + 'modules/amazonCrawler');
-var json2csv = require('json2csv');
 var fs = require('fs');
+var nodeExcel = require('excel-export');
 var permission = require(__base + 'modules/permission');
 router.use('/users', require(__base + 'routes/users'));
 router.use('/api', require(__base + 'routes/api'));
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
     if (!permission(req, res)) {
         return false;
     }
-    res.render('index',{"title" : "Index", "key":"", "user" : req.session.user, "permission":req.session.permission});
-
+    res.render('index', {
+        "title": "Index",
+        "key": "",
+        "user": req.session.user,
+        "permission": req.session.permission
+    });
 });
 router.get(/download\/(\w+)\/([^\/]+)/, function(req, res, next) {
     if (!permission(req, res)) {
         return false;
     }
     var id = req.params[0];
-    amazonCrawler.getAllComments(id,function(data){
-        try {
-          var result = json2csv({ data: data, fields: ["title","author","date","avp-badge","body","star","votes","images.0","images.1","images.2","images.3",] });
-          //res.set('Content-Type', 'text/csv');
-          //res.send(result);
-          fs.writeFileSync(__base + '/public/files/'+req.params[1],result);
-          res.redirect('/files/'+req.params[1]);
-        } catch (err) {
-          res.send(err);
+    amazonCrawler.getAllComments(id, function(data) {
+        var fields = ["title", "author", "date", "avp-badge", "body", "star", "votes", "images"];
+        var conf = {};
+        conf.name = "mysheet";
+        conf.cols = [];
+        for (i in fields) {
+            conf.cols.push({
+                caption: fields[i],
+                type: 'string',
+            });
         }
-
-
+        for (var x = 0; x < 9; x++) {
+            conf.cols.push({
+                caption: "",
+                type: 'string',
+            });
+        }
+        conf.rows = [];
+        for (i in data) {
+            var item = [];
+            for (j in fields) {
+                if (fields[j] == "images") {
+                    for (var x = 0; x < 10; x++) {
+                        item.push(data[i]["images"][x] || "");
+                    }
+                } else {
+                    item.push(data[i][fields[j]] || "");
+                }
+            }
+            conf.rows.push(item);
+        }
+        var result = nodeExcel.execute(conf);
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats');
+        res.setHeader("Content-Disposition", "attachment; filename=" + req.params[1]);
+        res.end(result, 'binary');
     });
-
 });
 module.exports = router;
